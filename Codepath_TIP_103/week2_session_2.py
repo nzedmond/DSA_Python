0049678e59d95b87d636e487f1b2c32b4b05f58b
Edmond Nzivugira <enzivugira@Edmonds-MacBook-Pro-4.local>
2026-09-24 21:28:48 -0400

    Verifying authenticity

--- Codepath_TIP_103/week2_session_2.py
+++ Codepath_TIP_103/week2_session_2.py
@@ -21,3 +21,31 @@ art_pieces3 = [1,1,1,1]
 print(find_balanced_subsequence(art_pieces1))
 print(find_balanced_subsequence(art_pieces2))
 print(find_balanced_subsequence(art_pieces3))  
+
+# Problem 2
+def is_authentic_collection(art_pieces):
+
+    if not art_pieces:
+        return False
+
+    n = len(art_pieces) - 1
+
+    # numbers 1 to n-1 appear once... n appears twice.
+    expected_count = {}
+    for i in range(1, n):
+        expected_count[i] = 1
+    expected_count[n] = 2
+
+    shipment_count = {}
+    for piece in art_pieces:
+        shipment_count[piece] = shipment_count.get(piece, 0) + 1
+
+    return shipment_count == expected_count
+
+collection1 = [2, 1, 3]
+collection2 = [1, 3, 3, 2]
+collection3 = [1, 1]
+
+print(is_authentic_collection(collection1))
+print(is_authentic_collection(collection2))
+print(is_authentic_collection(collection3))
